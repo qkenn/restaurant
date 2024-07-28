480430a86e988689a9bb37044b9c9e7550fe6a9c
qkenn <github.com.sampling990@passinbox.com>
2024-07-29 00:05:31 +0530

    feat: handle page render with standalone function

--- src/main.js
+++ src/main.js
@@ -6,6 +6,27 @@ import './styles/styles.css';
 const mainWrapper = document.getElementById('content');
 const btns = document.getElementById('btn-container');
 
+const handlePageRender = (currentPage) => {
+  let renderFunction = null;
+
+  switch (true) {
+    case currentPage == 'home':
+      renderFunction = initialRender;
+      break;
+    case currentPage == 'menu':
+      renderFunction = menuRender;
+      break;
+    case currentPage == 'about':
+      renderFunction = aboutRender;
+      break;
+    default:
+      renderFunction = initialRender;
+  }
+
+  mainWrapper.innerHTML = '';
+  mainWrapper.appendChild(renderFunction());
+};
+
 btns.addEventListener('click', (e) => {
   // event is only for btns
   if (!e.target.closest('.btn')) return;
@@ -15,16 +36,13 @@ btns.addEventListener('click', (e) => {
   // change page content based on btn
   switch (true) {
     case currentPage == 'home':
-      mainWrapper.innerHTML = '';
-      mainWrapper.appendChild(initialRender());
+      handlePageRender(currentPage);
       break;
     case currentPage == 'menu':
-      mainWrapper.innerHTML = '';
-      mainWrapper.appendChild(menuRender());
+      handlePageRender(currentPage);
       break;
     case currentPage == 'about':
-      mainWrapper.innerHTML = '';
-      mainWrapper.appendChild(aboutRender());
+      handlePageRender(currentPage);
       break;
     default:
       console.log('default');
