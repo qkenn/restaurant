7c9c0b430e07d8ea664c6b0d2252fe668615df61
qkenn <github.com.sampling990@passinbox.com>
2024-07-28 22:55:58 +0530

    feat: create aboutpage with javascript

--- src/scripts/aboutRender.js
+++ src/scripts/aboutRender.js
@@ -1,4 +1,5 @@
 import createTag from './helpers/createTag';
+import picture from '../images/authentic-asian-restaurant-interior.jpg';
 
 export default () => {
   const pageWrapper = createTag('div');
@@ -6,11 +7,29 @@ export default () => {
   const title = createTag('h2', [['class', 'page-title']]);
   title.textContent = 'About';
 
-  const paragraph = createTag('p', [['class', 'info']]);
-  paragraph.textContent = 'about content';
+  const imgContainer = createTag('div', [['class', 'img-container']]);
+  const img = createTag('img', [
+    ['id', 'page-img'],
+    ['alt', 'vibrant restaurant exterior at dusk'],
+    ['class', 'img'],
+    ['src', picture],
+  ]);
+  imgContainer.appendChild(img);
+
+  const description = createTag('div', [['class', 'description']]);
+
+  const headline = createTag('h3', [['class', 'headline']]);
+  headline.textContent = 'Experience the Art of Japanese Cuisine';
+  const headlineInfo = createTag('p', [['class', 'info']]);
+  headlineInfo.textContent =
+    "At Yume Ranch, we invite you to embark on a culinary adventure that will leave you feeling like you're dining in the heart of Japan. Come for the food, stay for the warm hospitality, and return for the unforgettable experience.";
+
+  description.appendChild(headline);
+  description.appendChild(headlineInfo);
 
   pageWrapper.appendChild(title);
-  pageWrapper.appendChild(paragraph);
+  pageWrapper.appendChild(imgContainer);
+  pageWrapper.appendChild(description);
 
   return pageWrapper;
 };
