85a7fbfedae4551f4004d9cf1108fadeee01a55e
qkenn <github.com.sampling990@passinbox.com>
2024-07-29 00:53:44 +0530

    refact: optimize main application logic with a class

--- src/main.js
+++ src/main.js
@@ -3,50 +3,61 @@ import aboutRender from './scripts/aboutRender';
 import menuRender from './scripts/menuRender';
 import './styles/styles.css';
 
-const mainWrapper = document.getElementById('content');
-const btns = document.getElementById('btn-container');
+class App {
+  mainWrapper = document.getElementById('content');
+  btns = document.getElementById('btn-container');
 
-const handlePageRender = (currentPage) => {
-  let renderFunction = null;
-
-  switch (true) {
-    case currentPage == 'home':
-      renderFunction = initialRender;
-      break;
-    case currentPage == 'menu':
-      renderFunction = menuRender;
-      break;
-    case currentPage == 'about':
-      renderFunction = aboutRender;
-      break;
-    default:
-      renderFunction = initialRender;
+  constructor() {
+    this.mainWrapper.appendChild(initialRender());
   }
 
-  mainWrapper.innerHTML = '';
-  mainWrapper.appendChild(renderFunction());
-};
-
-btns.addEventListener('click', (e) => {
-  // event is only for btns
-  if (!e.target.closest('.btn')) return;
-
-  const currentPage = e.target.dataset.id;
-
-  // change page content based on btn
-  switch (true) {
-    case currentPage == 'home':
-      handlePageRender(currentPage);
-      break;
-    case currentPage == 'menu':
-      handlePageRender(currentPage);
-      break;
-    case currentPage == 'about':
-      handlePageRender(currentPage);
-      break;
-    default:
-      console.log('default');
+  init() {
+    this.btns.addEventListener('click', this.handleEvent);
   }
-});
 
-mainWrapper.appendChild(initialRender());
+  handleEvent = (e) => {
+    // event is only for btns
+    if (!e.target.closest('.btn')) return;
+
+    const currentPage = e.target.dataset.id;
+
+    // change page content based on btn
+    switch (true) {
+      case currentPage == 'home':
+        this.handlePageRender(currentPage);
+        break;
+      case currentPage == 'menu':
+        this.handlePageRender(currentPage);
+        break;
+      case currentPage == 'about':
+        this.handlePageRender(currentPage);
+        break;
+      default:
+        console.log('default');
+    }
+  };
+
+  handlePageRender(currentPage) {
+    let renderFunction = null;
+
+    switch (true) {
+      case currentPage == 'home':
+        renderFunction = initialRender;
+        break;
+      case currentPage == 'menu':
+        renderFunction = menuRender;
+        break;
+      case currentPage == 'about':
+        renderFunction = aboutRender;
+        break;
+      default:
+        renderFunction = initialRender;
+    }
+
+    this.mainWrapper.innerHTML = '';
+    this.mainWrapper.appendChild(renderFunction());
+  }
+}
+
+const app = new App();
+app.init();
