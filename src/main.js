789b149cdb86345d36a825b37849668d83c2d28d
qkenn <github.com.sampling990@passinbox.com>
2024-07-28 19:37:49 +0530

    add homepage content & styling

--- src/main.js
+++ src/main.js
@@ -1,1 +1,4 @@
-console.log('hooked');
+import './styles/styles.css';
+import img from './images/vibrant-restaurant-exterior-at-dusk.jpg';
+
+document.getElementById('page-img').setAttribute('src', img);
