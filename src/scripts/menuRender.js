42909e17666e6701eb3bd82e13cc6c707fa63ff7
qkenn <github.com.sampling990@passinbox.com>
2024-07-28 22:49:05 +0530

    feat: create menupage with javascript

--- src/scripts/menuRender.js
+++ src/scripts/menuRender.js
@@ -1,4 +1,5 @@
 import createTag from './helpers/createTag';
+import picture from '../images/vibrant-casual-dining-experience.jpg';
 
 export default () => {
   const pageWrapper = createTag('div');
@@ -6,11 +7,45 @@ export default () => {
   const title = createTag('h2', [['class', 'page-title']]);
   title.textContent = 'Menu';
 
-  const paragraph = createTag('p', [['class', 'info']]);
-  paragraph.textContent = 'menu content';
+  const imgContainer = createTag('div', [['class', 'img-container']]);
+  const img = createTag('img', [
+    ['id', 'page-img'],
+    ['alt', 'vibrant restaurant exterior at dusk'],
+    ['class', 'img'],
+    ['src', picture],
+  ]);
+  imgContainer.appendChild(img);
+
+  const description = createTag('div', [['class', 'description']]);
+
+  const menu = createTag('ul', [['class', 'menu']]);
+
+  const item1 = createTag('li', [['class', 'menu-item']]);
+  item1.textContent = 'Appetizers';
+
+  const item2 = createTag('li', [['class', 'menu-item']]);
+  item2.textContent = 'Sushi & Sashimi';
+
+  const item3 = createTag('li', [['class', 'menu-item']]);
+  item3.textContent = 'Ramen and Udon';
+
+  const item4 = createTag('li', [['class', 'menu-item']]);
+  item4.textContent = 'Entrees';
+
+  const item5 = createTag('li', [['class', 'menu-item']]);
+  item5.textContent = 'Desserts';
+
+  const item6 = createTag('li', [['class', 'menu-item']]);
+  item6.textContent = 'Drinks';
+
+  const menuItems = [item1, item2, item3, item4, item5, item6];
+  menuItems.forEach((item) => menu.appendChild(item));
+
+  description.appendChild(menu);
 
   pageWrapper.appendChild(title);
-  pageWrapper.appendChild(paragraph);
+  pageWrapper.appendChild(imgContainer);
+  pageWrapper.appendChild(description);
 
   return pageWrapper;
 };
